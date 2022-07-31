d41c42e6ae327a3a692f28bb102eaa63813213ad
Jack Allen <jackallensemail+git@gmail.com>
2022-08-01 00:14:10 +0100

    release

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/houstn/terraform-provider-houstn/houstn"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: houstn.Provider,
