a36733b7507cefa2cc96c4704602641829fdc079
Jack Allen <jackallensemail+git@gmail.com>
2022-07-31 20:17:12 +0100

    init

---
+++ houstn/provider.go
@@ -0,0 +1,9 @@
+package houstn
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: providerSchema(),
+	}
+}
---
+++ houstn/schema.go
@@ -0,0 +1,7 @@
+package houstn
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+func providerSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{}
+}
